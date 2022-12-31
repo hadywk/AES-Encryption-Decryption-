81288277b8e46f6727e0590ccc0e2bfd60c1ff8f
Bilal Ghareeb <79377889+Bilal-Ghareeb@users.noreply.github.com>
2022-12-31 22:08:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,31 @@ message_label.grid(row=1, column=0)
 # Create an input field for the message
 message_entry = Entry(window, width=100)
 message_entry.grid(row=1, column=1)
+
+
+# Create a function to perform encryption
+def encrypt():
+    # Get the key and message from the input fields
+    key = key_entry.get()
+    message = message_entry.get()
+
+    # Pad the message to be a multiple of 16 bytes
+    message = message + " " * (16 - (len(message) % 16))
+
+    # Convert the key and message to bytes
+    key = key.encode()
+    message = message.encode()
+
+    # Create the AES cipher object
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    # Encrypt the message 10 times
+    for i in range(10):
+        message = cipher.encrypt(message)
+
+    # Encode the encrypted message as a base64 string
+    encrypted_message = base64.b64encode(message).decode()
+
+    # Update the message input field with the encrypted message
+    message_entry.delete(0, 'end')
+    message_entry.insert(0, encrypted_message)
